c5c99c34f10ae26dfb64f9824f14cc693d32d82c
zetahernandez <zetahernandez@gmail.com>
2013-09-27 03:18:28 -0300

    carousel behaivor and style

--- public/app/scripts/views/carousel.js
+++ public/app/scripts/views/carousel.js
@@ -1,26 +1,33 @@
 BooksManager.CarouselView = Ember.View.extend({
   templateName: 'carousel',
-  classNames: ['jcarousel-skin-tango','width'],
+  layoutName: 'carousel-layout',
+  classNames: ['carousel'],
   init: function () {
     this._super.apply(this, arguments);
     var obj = this.get('content.firstObject');
     // Ember.set(obj, 'isActive', true);
   },
   actions: {
-    
+
   },
   didInsertElement: function () {
-    this.$().jcarousel();
+    this.$('.jcarousel-skin-tango').jcarousel();
   },
   itemsView: Ember.CollectionView.extend({
     tagName: 'ul',
     classNames: ['jcarousel-list'],
+
     contentBinding: 'parentView.content',
     itemViewClass: Ember.View.extend({
       tagName: 'li',
       classNames: ['jcarousel-item'],
       // classNameBindings: ['content.isActive:active'],
-      templateName: 'carousel-item'
+      templateName: 'carousel-item',
+      didInsertElement: function () {
+        this.$('a[data-toggle="tooltip"]').tooltip({
+          placement: 'auto bottom'
+        });
+      },
     })
   })
 });
