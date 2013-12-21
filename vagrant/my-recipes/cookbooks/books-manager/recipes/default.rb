36592b01e3bc6275eb2cf988758d050c1f5c178a
Fernando Hernandez <zetahernandez@gmail.com>
2013-12-21 15:46:33 -0300

    evit reinstall package for global dependecies, add new recipe for it

--- vagrant/my-recipes/cookbooks/books-manager/recipes/default.rb
+++ vagrant/my-recipes/cookbooks/books-manager/recipes/default.rb
@@ -13,23 +13,7 @@ if node[:disableIPv6]
    end
 end
 
-execute "Installing sass" do
-  command "gem install sass -v 3.2.10"
-end
-
-execute "Installing compass" do
-  command "gem install compass"
-end
-
-execute "Installing grunt-contrib-compass" do
-  command "npm install -g grunt-contrib-compass"
-  only_if { node[:environment] == 'vagrant' }
-end
-
-execute "Installing mocha grunt-cli grunt-mocha" do
-  command "npm install -g mocha grunt grunt-cli grunt-mocha"
-end
-
+include_recipe "books-manager::global-dependencies"
 # Do an npm rebuild for compiled dependencies.
 
 #TODO: run npm install when node_modules not exist, otherwise run npm rebuild
---
+++ vagrant/my-recipes/cookbooks/books-manager/recipes/global-dependencies.rb
@@ -0,0 +1,42 @@
+
+########################################################
+#  GLOBAL DEPENDENCIES                                 #
+########################################################
+
+
+execute "Installing sass" do
+  #Use sass 3.2.10 * with version 3.2.12 doesn't work
+  command "gem install sass -v 3.2.10"
+  not_if do ::File.exists?('/opt/vagrant_ruby/bin/sass') end
+end
+
+execute "Installing compass" do
+  command "gem install compass"
+  not_if do ::File.exists?('/opt/vagrant_ruby/bin/compass') end
+end
+
+execute "Installing grunt-contrib-compass" do
+  command "npm install -g grunt-contrib-compass"
+  only_if { node[:environment] == 'vagrant' }
+  not_if do ::File.exists?('/usr/local/lib/node_modules/grunt-contrib-compass') end
+end
+
+execute "Installing mocha" do
+  command "npm install -g mocha"
+  not_if do ::File.exists?('/usr/local/bin/mocha') end
+end
+
+execute "Installing grunt" do
+  command "npm install -g grunt grunt-cli "
+  not_if do ::File.exists?('/usr/local/bin/grunt') end
+end
+
+execute "Installing grunt-cli" do
+  command "npm install -g grunt-cli"
+  not_if do ::File.exists?('/usr/local/lib/node_modules/grunt-cli') end
+end
+
+execute "Installing grunt-mocha" do
+  command "npm install -g grunt-mocha"
+  not_if do ::File.exists?('/usr/local/lib/node_modules/grunt-mocha') end
+end
